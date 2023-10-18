f5259acf46a4807499e1747120fa6b0929de457e
VinMendes <vinicius.mc6@puccampinas.edu.br>
2023-10-18 10:30:42 -0300

    MAIN

---
+++ jogo_anagrama.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <iomanip>
+using namespace std;
+#define MAX 30
+
+int main () {
+    char vetorPalavras[MAX][15];
+    char vetorLetras[MAX];
+    char vetorRespostas[MAX][15];
+}
