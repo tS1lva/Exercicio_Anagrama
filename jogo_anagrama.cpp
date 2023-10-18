5793a2e12a8fdd969c51f659305a5d6456a192e0
megaManX6 <tiagu.pereira@hotmail.com>
2023-10-18 10:32:17 -0300

    teste

---
+++ jogo_anagrama.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <iomanip>
+using namespace std;
+#define MAX 30
+
+
+void imprime_anagrama(char *vetor_anagrama){
+
+    for (int i = 0 ; i < MAX ; i++){
+
+
+    }
+};
+
+int main(){
+    char vetor_anagrama[];
+
+    return 0;
+};
