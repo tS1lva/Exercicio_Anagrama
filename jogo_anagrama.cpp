45ed1d744e9104880de933d40c37a477be4e6598
VinMendes <vinicius.mc6@puccampinas.edu.br>
2023-10-18 11:32:11 -0300

    v

--- jogo_anagrama.cpp
+++ jogo_anagrama.cpp
@@ -17,7 +17,7 @@ void imprime_anagrama(char *vetor_anagrama){
     }
 };
 
-int confere (char *vetorPalavras[MAX], char resposta[15]) {
+int confere (char vetorPalavras[][15], char resposta[15]) {
     for(int ax = 0; ax < MAX; ax++) {
         if (strcmp(vetorPalavras[ax], resposta) == 0) {
             return 0;
@@ -27,6 +27,10 @@ int confere (char *vetorPalavras[MAX], char resposta[15]) {
 }
 
 int main () {
+    int contaErro = 1;
+    int contaAcerto = 0;
+    int flag;
+    char resposta[15];
     char vetorPalavras[MAX][15] = {
     "Bolacha", "Jato", "Rato", "Bota", "Brio", "Corte", "Caju", "Outra", "Oito", "Rio", "Trio", "Rua", "Tia", "Lua", "Rota", "Boia", "Taco", "Rico", "Bote", "Tijolo"
     };
@@ -34,6 +38,21 @@ int main () {
     char vetorLetras[20] = {'A','B','C','D','A','E','I','O','U','D','B','J','R','T','U','J','B','T','L','L'};
     char vetorRespostas[MAX][15];
     imprime_anagrama(vetorLetras);
+    do {
+        cout<<"Nome: ";
+        cin.getline(resposta, 15);
+        flag = confere(vetorPalavras, resposta);
+        if (flag == 0) {
+            strcpy(vetorRespostas[contaAcerto], resposta);
+            contaAcerto++;
+        }
+        else {
+            contaErro++;
+        }
+    }while (contaErro < 6);
+    for(int ax = 0; ax < contaAcerto; ax++) {
+        cout<<vetorRespostas[ax]<<endl;
+    }
 
-
+    return 0;
 }
