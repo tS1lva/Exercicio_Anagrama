8e1a0406a89366e00e7eff8d4b990a4b691303a3
megaManX6 <tiagu.pereira@hotmail.com>
2023-10-18 11:32:54 -0300

    t

--- jogo_anagrama.cpp
+++ jogo_anagrama.cpp
@@ -17,7 +17,7 @@ void imprime_anagrama(char *vetor_anagrama){
     }
 };
 
-int confere (char *vetorPalavras[MAX], char resposta[15]) {
+int confere (char vetorPalavras[][15], char resposta[15]) {
     for(int ax = 0; ax < MAX; ax++) {
         if (strcmp(vetorPalavras[ax], resposta) == 0) {
             return 0;
@@ -30,10 +30,32 @@ int main () {
     char vetorPalavras[MAX][15] = {
     "Bolacha", "Jato", "Rato", "Bota", "Brio", "Corte", "Caju", "Outra", "Oito", "Rio", "Trio", "Rua", "Tia", "Lua", "Rota", "Boia", "Taco", "Rico", "Bote", "Tijolo"
     };
-    
+
     char vetorLetras[20] = {'A','B','C','D','A','E','I','O','U','D','B','J','R','T','U','J','B','T','L','L'};
     char vetorRespostas[MAX][15];
+    char resposta[15];
+    int conta_erros = 0;
+    int erro = 0;
+
+    
     imprime_anagrama(vetorLetras);
 
+    do
+    {
+        cout<<"\nDigite uma Palavra: ";
+        cin.getline(resposta, 15);
+
+        erro = confere(vetorPalavras, resposta);
+
+        if(erro){
+            conta_erros += 1;
+            cout<<"\nErrou!";
+        }else{
+            cout<<"\nAcertou!";
+        }
+
+            
+    } while ((conta_erros < 6) || (erro = 1));
+    
 
 }
