e0d9a314bc699a526d988c129e0c7a358944359a
megaManX6 <tiagu.pereira@hotmail.com>
2023-10-18 10:42:16 -0300

    t

--- jogo_anagrama.cpp
+++ jogo_anagrama.cpp
@@ -9,13 +9,16 @@ using namespace std;
 void imprime_anagrama(char *vetor_anagrama){
 
     for (int i = 0 ; i < MAX ; i++){
-
+        cout<<vetor_anagrama[i];
+        if (i%4 == 0){
+            cout<<endl;
+        }
 
     }
 };
 
 int main () {
     char vetorPalavras[MAX][15];
-    char vetorLetras[MAX];
+    char vetorLetras[20];
     char vetorRespostas[MAX][15];
 }
